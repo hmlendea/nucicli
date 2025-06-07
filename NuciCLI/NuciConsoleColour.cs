ae940a9297036cfa5ee58b2b3ea8ecf901ca1e18
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-06-07 13:21:09 +0300

    refactor: Refactored the code to newer standards

--- NuciCLI/NuciConsoleColour.cs
+++ NuciCLI/NuciConsoleColour.cs
@@ -10,30 +10,30 @@ namespace NuciCLI
 
         public string Name { get; set; }
 
-        public static NuciConsoleColour Default => new NuciConsoleColour(-1, nameof(Default));
-        public static NuciConsoleColour Black => new NuciConsoleColour(0, nameof(Black));
-        public static NuciConsoleColour DarkBlue => new NuciConsoleColour(1, nameof(DarkBlue));
-        public static NuciConsoleColour DarkGreen => new NuciConsoleColour(2, nameof(DarkGreen));
-        public static NuciConsoleColour DarkCyan => new NuciConsoleColour(3, nameof(DarkCyan));
-        public static NuciConsoleColour DarkRed => new NuciConsoleColour(4, nameof(DarkRed));
-        public static NuciConsoleColour DarkMagenta => new NuciConsoleColour(5, nameof(DarkMagenta));
-        public static NuciConsoleColour DarkYellow => new NuciConsoleColour(6, nameof(DarkYellow));
-        public static NuciConsoleColour Gray => new NuciConsoleColour(7, nameof(Gray));
-        public static NuciConsoleColour DarkGray => new NuciConsoleColour(8, nameof(DarkGray));
-        public static NuciConsoleColour Blue => new NuciConsoleColour(9, nameof(Blue));
-        public static NuciConsoleColour Green => new NuciConsoleColour(10, nameof(Green));
-        public static NuciConsoleColour Cyan => new NuciConsoleColour(11, nameof(Cyan));
-        public static NuciConsoleColour Red => new NuciConsoleColour(12, nameof(Red));
-        public static NuciConsoleColour Magenta => new NuciConsoleColour(13, nameof(Magenta));
-        public static NuciConsoleColour Yellow => new NuciConsoleColour(14, nameof(Yellow));
-        public static NuciConsoleColour White => new NuciConsoleColour(15, nameof(White));
+        public static NuciConsoleColour Default => new(-1, nameof(Default));
+        public static NuciConsoleColour Black => new(0, nameof(Black));
+        public static NuciConsoleColour DarkBlue => new(1, nameof(DarkBlue));
+        public static NuciConsoleColour DarkGreen => new(2, nameof(DarkGreen));
+        public static NuciConsoleColour DarkCyan => new(3, nameof(DarkCyan));
+        public static NuciConsoleColour DarkRed => new(4, nameof(DarkRed));
+        public static NuciConsoleColour DarkMagenta => new(5, nameof(DarkMagenta));
+        public static NuciConsoleColour DarkYellow => new(6, nameof(DarkYellow));
+        public static NuciConsoleColour Gray => new(7, nameof(Gray));
+        public static NuciConsoleColour DarkGray => new(8, nameof(DarkGray));
+        public static NuciConsoleColour Blue => new(9, nameof(Blue));
+        public static NuciConsoleColour Green => new(10, nameof(Green));
+        public static NuciConsoleColour Cyan => new(11, nameof(Cyan));
+        public static NuciConsoleColour Red => new(12, nameof(Red));
+        public static NuciConsoleColour Magenta => new(13, nameof(Magenta));
+        public static NuciConsoleColour Yellow => new(14, nameof(Yellow));
+        public static NuciConsoleColour White => new(15, nameof(White));
 
         NuciConsoleColour(int value, string name)
         {
             Value = value;
             Name = name;
 
-            if (Value == -1)
+            if (Value.Equals(-1))
             {
                 ConsoleColour = null;
             }
